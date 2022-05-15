fe80e1164b3ea5650186c5499552aa4f7aa84181
thiagoleites <thiagoleites@outlook.com.br>
2022-05-15 01:52:01 -0300

    First Commit

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,13 @@
-import { FormControl, Input } from '@chakra-ui/react'
+import { FormControl, Input, FormLabel, FormErrorMessage } from '@chakra-ui/react'
 
 
-export const InputForm = ({ label, name, ...rest }) => {
+export const InputForm = ({ label, name, error = null, ...rest }) => {
 
   return (
-    <FormControl id="email" marginY="2rem">
-      <formLabel>{label}</formLabel>
+    <FormControl id="email" marginY="2rem" isInvalid={!!error}>
+      <FormLabel>{label}</FormLabel>
       <Input name={name} id={name} {...rest} />
+      {!!error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
 
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,30 +20,58 @@ import { InputForm } from '../components/Input';
 export default function Home() {
 
   const [clients, setClients] = useState([])
+  const [isFormOpen, setIsFormOpen] = useState(false)
+
   const [id, setId] = useState(null)
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const [errors, setErrors] = useState({name: null, email: null})
+
+  const isValidFormData = () => {
+    if(!name) {
+      setErrors({name: 'Esse campo é obrigatório'})
+      return false
+    }
+    if(!email) {
+      setErrors({email: 'Esse campo é obrigatório'})
+      return false
+    }
+    if(clients.some(client => client.email === email && client.id !== id)) {
+      setErrors({email: "O email informado já está em uso!"})
+      return
+    }
+    setErrors({})
+    return true
+  }
+
   const handleSubmitCreate = (e) => {
     e.preventDefault();
-    if(!name && !email) return
+    if(!isValidFormData()) return
 
     setClients(clients.concat({_id: new Date().getUTCMilliseconds().toString(), name, email}))
-    // setClients(clients.map(client => client._id === id ? {name, email, _id:id} : client))
 
     setName('')
     setEmail('')
+    toggleFormStat()
   }
 
   const handleSubmitUpdateClient = (e) => {
     e.preventDefault();
+    if(!isValidFormData()) return
+
     setClients(clients.map(client => client._id === id ? {name, email, _id:id} : client))
+
+    setName('')
+    setEmail('')
+    setId(null)
+    toggleFormStat()
   }
 
   const handleDeleteClient = (_id) => {
     setClients(clients.filter(client => client.id !== _id))
   }
-
 
   const handleChangeName = (text) => {
     setName(text);
@@ -56,6 +84,11 @@ export default function Home() {
     setId(client._id);
     setName(client.name);
     setEmail(client.email);
+    setIsFormOpen(true)
+  }
+
+  const toggleFormStat = () => {
+    setIsFormOpen(!isFormOpen)
   }
 
   return (
@@ -66,34 +99,40 @@ export default function Home() {
       <Flex color="white" justifyContent='space-between' margin='5' padding='10'>
 
           <Text color='black' fontSize="3xl">Lista de usuários</Text>
-      <Button colorScheme='blue'>+</Button>
+      <Button colorScheme='blue' onClick={toggleFormStat}>{isFormOpen ? '-' : '+'}</Button>
       </Flex>
-      <VStack marginY="1rem" as="form" onSubmit={id ? handleSubmitUpdateClient :  handleSubmitCreate}>
-        <InputForm 
-          label="Nome" 
-          name="name" 
-          value={name} 
-          type="text" 
-          placeholder="Digite seu nome" 
-          onChange={e => handleChangeName(e.target.value)} 
-        />
+      { isFormOpen && (
 
-        <InputForm 
-          label="Email" 
-          name="email" 
-          value={email} 
-          type="email" 
-          placeholder="Digite seu email" 
-          onChange={e => handleChangeEmail(e.target.value)} 
-        />
+        <VStack marginY="1rem" as="form" onSubmit={id ? handleSubmitUpdateClient :  handleSubmitCreate}>
+          <InputForm 
+            label="Nome" 
+            name="name" 
+            value={name} 
+            type="text" 
+            placeholder="Digite seu nome" 
+            onChange={e => handleChangeName(e.target.value)}
+            error={errors.name}
+          />
 
-        <Button fontSize="sm" marginY="5" color="white" type="submit" colorScheme="blue" alignSelf="flex-end">
-          {id ? 'Atualizar': 'Cadastrar'}
-        </Button>
+          <InputForm 
+            label="Email" 
+            name="email" 
+            value={email} 
+            type="email" 
+            placeholder="Digite seu email" 
+            onChange={e => handleChangeEmail(e.target.value)} 
+            error={errors.email}
+          />
+
+          <Button fontSize="sm" marginY="5" color="white" type="submit" colorScheme="blue" alignSelf="flex-end">
+            {id ? 'Atualizar': 'Cadastrar'}
+          </Button>
       </VStack>
+      )}
+
 
       <TableContainer>
-      <Table variant='simple'>
+      <Table variant='simple' my="10">
         <TableCaption>Dados cadastrados</TableCaption>
         <Thead bgColor="blue.200">
           <Tr>
